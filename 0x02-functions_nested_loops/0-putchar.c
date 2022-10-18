f81e5e6cbb428a02ba6d214b0b3ba720fa3c61a6
4honore <ishimwehonore450@gmail.com>
2022-10-18 07:16:50 -0700

    _putchar_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
-#iclude "main.h"
+#include "main.h"
+#include <stdio.h>
 /**
  * main - all programs must contain main function
  * Return: Always 0
  */
 int main(void)
 {
-	putchar('_');
+	char B ='_';
+	putchar(B);
 	putchar('p');
 	putchar('u');
 	putchar('t');
