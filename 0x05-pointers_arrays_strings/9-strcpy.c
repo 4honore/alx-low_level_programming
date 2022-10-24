46f147a777d92ce9a0b55c6659a36d5ddf97c9be
4honore <ishimwehonore450@gmail.com>
2022-10-24 06:09:49 -0700

    converts a string to interger

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,12 +6,13 @@
  * @src: The source string to copy.
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, const char *src)
+char* _strcpy(char *dest, const char *src)
 {
 	int index = 0;
 
 	while (src[index])
 	{
+
 		dest[index] = src[index];
 		index++;
 	}
