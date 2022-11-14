93fde3f5939a5f975f1ab00aff2adba2b67b974f
4honore <ishimwehonore450@gmail.com>
2022-11-14 11:35:55 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog -dog attributes
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog
+ *
+ * Decription: the attributes of a dog.
+ */
 struct dog
 {
 	char *name;
